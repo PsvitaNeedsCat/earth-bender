f783836936cfd207aa7bbd7a43782545038d492a
sebbyling <43594362+sebbyling@users.noreply.github.com>
2020-02-25 15:57:29 +1300

    Added water shader
    Added a placeholder water shader made available for use by Linden Reid

---
+++ Assets/Shaders/NewCameraScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class NewCameraScript : MonoBehaviour
+{
+
+    private Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.depthTextureMode = DepthTextureMode.Depth;
+    }
+
+}

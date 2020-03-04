28ec52278893c09af4b967b5bee65d8a7326095e
sebbyling <43594362+sebbyling@users.noreply.github.com>
2020-03-04 17:40:02 +1300

    Pixelate
    Pixelation attached to cameras

---
+++ Assets/Shaders/PixelateEffect_SCR.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PixelateEffect_SCR : MonoBehaviour
+{
+    public Material effectMat;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, effectMat);
+    }
+}

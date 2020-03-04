f2b262f53d2e9b02870b7568b223c1fa511014c9
Callum <53197859+PsvitaNeedsCat@users.noreply.github.com>
2020-03-05 10:09:44 +1300

    Basic Game Loop

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -11,41 +11,13 @@ public class DoorTrigger : MonoBehaviour
         if (triggered) { return; }
         triggered = true;
 
-        Player _player = GameObject.FindObjectOfType<Player>();
         Animator _fade = GameObject.Find("Fade").GetComponent<Animator>();
-
-        // Remove controls
-        _player.SetControls(false);
-
-        // Make player kinematic
-        _player.GetComponent<Rigidbody>().isKinematic = true;
 
         // Fade to black
         _fade.SetTrigger("FadeOut");
 
         // Load next level
         GameObject.FindObjectOfType<SceneController>().LoadNextLevel();
-
-        // Move player position
-        SpawnPosition[] spawnPositions = GameObject.FindObjectsOfType<SpawnPosition>();
-        if (spawnPositions.Length == 1) { _player.transform.position = spawnPositions[0].transform.position; }
-        else
-        {
-            int highestLevel = 0;
-            int index = -1;
-            for (int i = 0; i < spawnPositions.Length; i++)
-            {
-                if (spawnPositions[i].level > highestLevel) { highestLevel = spawnPositions[i].level; index = i; }
-            }
-
-            _player.transform.position = spawnPositions[index].transform.position;
-        }
-
-        // Make player non-kinematic
-        _player.GetComponent<Rigidbody>().isKinematic = false;
-
-        // Give player control again
-        _player.SetControls(true);
 
         _fade.SetTrigger("FadeIn");
     }

69541ddb15995b6dd91cfbaae07b48897c9727b9
Callum <53197859+PsvitaNeedsCat@users.noreply.github.com>
2020-03-09 09:43:18 +1300

    Music

---
+++ Assets/Scripts/Temp/TempMusicManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempMusicManager : MonoBehaviour
+{
+    AudioSource _audioSource;
+
+    [SerializeField] AudioClip bossMusic;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayBossMusic()
+    {
+        _audioSource.Stop();
+        _audioSource.clip = bossMusic;
+        _audioSource.Play();
+    }
+}

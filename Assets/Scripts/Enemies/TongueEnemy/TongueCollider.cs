e674c74858ee3245c7ee8d245c0df757df62d259
Callum <53197859+PsvitaNeedsCat@users.noreply.github.com>
2020-03-09 16:59:46 +1300

    Tongue Enemy

---
+++ Assets/Scripts/Enemies/TongueEnemy/TongueCollider.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class TongueCollider : MonoBehaviour
+{
+    public int damage = 1;
+    public TongueEnemy tongueEnemy;
+    private Chunk attachedChunk = null;
+    public Transform tweenTransform;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+
+        if (playerHealth)
+        {
+            playerHealth.Damage(damage);
+            Retract();
+
+            return;
+        }
+
+        if (!attachedChunk)
+        {
+            Chunk chunk = other.GetComponent<Chunk>();
+
+            if (chunk)
+            {
+                // Pick up & eat chunk
+                AudioManager.Instance.PlaySoundVaried("stuck");
+
+                chunk.Detach();
+
+                chunk.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                chunk.transform.parent = this.transform;
+                chunk.GetComponent<Collider>().isTrigger = true;
+
+                attachedChunk = chunk;
+
+                Retract();
+                return;
+            }
+        }
+    }
+
+    public void Extend()
+    {
+        tongueEnemy.isExtending = true;
+
+        // Tween to tweenPosition
+        transform.DOKill(false);
+        transform.DOMove(tweenTransform.position, 1.0f).OnComplete(() => Retract());
+
+        Debug.Log("Extending");
+    }
+
+    void Retract()
+    {
+        tongueEnemy.isExtending = false;
+        tongueEnemy.isRetracting = true;
+
+        // Tween back to TongueEnemy
+        transform.DOKill(false);
+        transform.DOMove(tongueEnemy.transform.position, 1.0f).OnComplete(() => tongueEnemy.Swallow());
+
+        Debug.Log("Retracting");
+    }
+
+    public GroundType Swallow()
+    {
+        if (!attachedChunk) { return GroundType.none; }
+
+        GroundType type = attachedChunk.type;
+
+        attachedChunk.DestroyQuiet();
+
+        return type;
+    }
+}
---
+++ Assets/Scripts/Enemies/TongueEnemy/TongueEnemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TongueEnemy : MonoBehaviour
+{
+    public TongueCollider tongueCollider;
+
+    public bool isExtending = false;
+    public bool isRetracting = false;
+
+    float extendTimer;
+    const float maxExtendTimer = 2.0f;
+
+    private void Awake()
+    {
+        extendTimer = maxExtendTimer;
+    }
+
+    public void Swallow()
+    {
+        isRetracting = false;
+
+        GroundType typeSwallowed = tongueCollider.Swallow();
+        Debug.Log("Enemy swallowed: " + typeSwallowed.ToString());
+
+        if (typeSwallowed == GroundType.poison)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        tongueCollider.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!isExtending && !isRetracting)
+        {
+            extendTimer -= Time.deltaTime;
+            if (extendTimer <= 0.0f)
+            {
+                extendTimer = maxExtendTimer;
+                tongueCollider.gameObject.SetActive(true);
+                tongueCollider.Extend();
+            }
+        }
+    }
+}
